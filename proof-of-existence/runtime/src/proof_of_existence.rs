e4011fca36f19b0d015c82f9badeb5ef644ea165
Shawn Tabrizi <shawntabrizi@gmail.com>
2018-12-31 13:23:37 -0800

    Add fee to claim, return fee on revoke

--- proof-of-existence/runtime/src/proof_of_existence.rs
+++ proof-of-existence/runtime/src/proof_of_existence.rs
@@ -3,6 +3,8 @@ use rstd::vec::Vec;
 use system::ensure_signed;
 use runtime_primitives::traits::As;
 
+const POE_FEE: u64 = 1000;
+
 pub trait Trait: timestamp::Trait + balances::Trait {
     type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
@@ -13,21 +15,28 @@ decl_module! {
 
         fn create_claim(origin, digest: Vec<u8>) -> Result {
             ensure!(!<Proofs<T>>::exists(&digest), "This digest has already been claimed");
+
             let sender = ensure_signed(origin)?;
             let time = <timestamp::Module<T>>::now();
 
-            <balances::Module<T>>::decrease_free_balance(&sender, <T::Balance as As<u64>>::sa(1000))?;
+            <balances::Module<T>>::decrease_free_balance(&sender, <T::Balance as As<u64>>::sa(POE_FEE))?;
             <Proofs<T>>::insert(&digest, (sender.clone(), time.clone()));
+
             Self::deposit_event(RawEvent::ClaimCreated(sender, time, digest));
             Ok(())
         }
 
         fn revoke_claim(origin, digest: Vec<u8>) -> Result {
             ensure!(<Proofs<T>>::exists(&digest), "This digest has not been claimed yet");
+
             let sender = ensure_signed(origin)?;
             let (owner, _time) = Self::proofs(&digest);
+
             ensure!(sender == owner, "You must own this claim to revoke it");
+
             <Proofs<T>>::remove(&digest);
+            <balances::Module<T>>::increase_free_balance_creating(&sender, <T::Balance as As<u64>>::sa(POE_FEE));
+
             Self::deposit_event(RawEvent::ClaimRevoked(sender, digest));
             Ok(())
         }
